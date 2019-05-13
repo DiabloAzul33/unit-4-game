83878376f1f66cf96d915e63d97410a464a87e9c
DiabloAzul <curr33@gmail.com>
2019-05-12 23:56:55 -0600

    .empty() the target/current score but startGame(); won't reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var targetScore = Math.round(Math.random() * (120 - 19) + 19); // randomized variable for target # player is trying to achieve (b/w 19-120)
+var targetScore = Math.round(Math.random() * (120 - 19) + 19); // randomized target # player is trying to achieve (b/w 19-120)
 $("#target-number").text(targetScore); // displays target # on page
 
 var counter = 0; // value points will increase by different increments here, starts at zero
@@ -14,15 +14,16 @@ function generateRandomNumber(){ // generates # from 1-12 for coin values each r
     return Math.floor(Math.random() * 12 +1);
 }
 
-function initializeGame(){ // sets random # for each coin 
+function startGame(){ // sets random # for each coin (1-12)
     goldCoin = generateRandomNumber();
     blueCoin = generateRandomNumber();
     redCoin = generateRandomNumber();
     rainbowCoin = generateRandomNumber();
 }
-initializeGame(); // calls for game to start by running fxn above
 
-function updateScore(){  // adds into HTML new current score counter
+startGame(); // calls for game to start by running fxn above
+
+function updateScore(){  // displays into HTML new current score counter
     $("#current-score").text(counter);
     $("#win-record").text(wins);
     $("#loss-record").text(losses);
@@ -30,15 +31,19 @@ function updateScore(){  // adds into HTML new current score counter
 
 function checkScore(){
     if (counter === targetScore) {
-        wins++;
-        $("#win-record").text(wins);
+        wins++; // adds 1 to win counter
+        $("#win-record").text(wins); // displays in HTML
         alert("YOU WIN! You are SUPERB!!");  // hit target score, alert win
-        initializeGame();
+        $("#target-number").empty(); // clears target score 
+        $("#current-score").empty(); // clears current score
+        startGame(); // restart game
     } else if (counter > targetScore) {
-        losses++;
-        $("#loss-record").text(losses);
+        losses++; // adds 1 to loss counter
+        $("#loss-record").text(losses); // displays in HTML
         alert("GAME OVER, better luck next time!!"); // goes above target score, alert loss
-        initializeGame();
+        $("#target-number").empty();
+        $("#current-score").empty();
+        startGame(); // restart game
     }  
 }
 
